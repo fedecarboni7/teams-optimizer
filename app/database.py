5cdd907cb4cb27e6b8656991c31a3e458da4805a
fedecarboni7 <fedecarboni7@gmail.com>
2024-07-02 16:55:54 +0000

    cambio de bcrypt a PBKDF2

---
+++ app/database.py
@@ -0,0 +1,63 @@
+import os
+
+from passlib.hash import pbkdf2_sha256
+
+from sqlalchemy import ForeignKey, create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
+TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+
+dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+
+engine = create_engine(dbUrl, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    players = relationship("Player", back_populates="user")
+
+    def set_password(self, password):
+        self.password = pbkdf2_sha256.hash(password)
+
+    def verify_password(self, password):
+        return pbkdf2_sha256.verify(password, self.password)
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    velocidad = Column(Integer)
+    resistencia = Column(Integer)
+    control = Column(Integer)
+    pases = Column(Integer)
+    tiro = Column(Integer)
+    defensa = Column(Integer)
+    habilidad_arquero = Column(Integer)
+    fuerza_cuerpo = Column(Integer)
+    vision = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="players")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Crear las tablas
+Base.metadata.create_all(bind=engine)
