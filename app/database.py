3b0c75edfacc52ca646f7d51e903010cf6eeef37
fedecarboni7 <fedecarboni7@gmail.com>
2024-07-03 23:36:51 -0300

    chore: Update database connection timeout configuration

--- app/database.py
+++ app/database.py
@@ -11,7 +11,8 @@ TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
 
 dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
 
-engine = create_engine(dbUrl, connect_args={'check_same_thread': False})
+timeout = int(os.getenv('DB_TIMEOUT', 30))
+engine = create_engine(dbUrl, connect_args={'check_same_thread': False, 'timeout': timeout})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
