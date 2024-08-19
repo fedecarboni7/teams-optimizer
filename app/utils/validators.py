af67eb973ac3d36569cbaa9ea1d457262c33d1e1
fedecarboni7 <fedecarboni7@gmail.com>
2024-08-19 19:16:00 -0300

    chore: Improve password validation and requirements in signup form

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -16,13 +16,5 @@ def validate_password(password: str):
         raise ValueError("La contraseña debe contener al menos una letra minúscula.")
     if not re.search(r"[0-9]", password):
         raise ValueError("La contraseña debe contener al menos un número.")
-    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
-        raise ValueError("La contraseña debe contener al menos un carácter especial.")
-    if re.search(r"(.)\1\1\1", password):
-        raise ValueError("La contraseña no debe contener más de tres caracteres repetidos consecutivos.")
-    if re.search(r"(012|123|234|345|456|567|678|789|890|qwerty|asdf)", password.lower()):
-        raise ValueError("La contraseña no debe contener secuencias de números o teclas comunes.")
-
-    common_passwords = ["123456", "password", "12345678", "qwerty", "abc123"]
-    if password.lower() in common_passwords:
-        raise ValueError("La contraseña es demasiado común. Por favor, elija una diferente.")
+    if len(password) < 12 and not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        raise ValueError("La contraseña de menos de 12 caracteres debe contener al menos un carácter especial.")
