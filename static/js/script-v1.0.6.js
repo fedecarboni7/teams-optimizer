731098a6216d2329d898a43eb3731ce39b2546f1
fedecarboni7 <fedecarboni7@gmail.com>
2024-09-02 21:32:05 -0300

    chore: Update select all players button  when added player deleted

--- static/js/script-v1.0.6.js
+++ static/js/script-v1.0.6.js
@@ -310,6 +310,7 @@ function addPlayer() {
         container.removeChild(playerDiv);
         renumerarJugadores();
         updateSelectedCount();
+        updateToggleButtonText();
     });
     playerHeader.appendChild(deleteButton);
 
