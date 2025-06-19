9a77793031c0896bd43bf448e3bb3e2b3d025400
fedecarboni7 <fedecarboni7@gmail.com>
2025-06-18 23:48:59 -0300

    refactor: Eliminar variables de configuración innecesarias en la clase Settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,9 +10,6 @@ class Settings:
         self.database_url = os.getenv("DATABASE_URL")
         self.secret_key = os.getenv("SECRET_KEY")
         self.logging_level = os.getenv("LOGGING_LEVEL", "INFO")
-        self.local_db = os.getenv("LOCAL_DB").lower() == "true"
-        self.db_timeout = int(os.getenv("DB_TIMEOUT", 30))
-        self.google_api_key = os.getenv("GOOGLE_API_KEY")
         self.smtp_server = os.getenv("SMTP_SERVER")
         self.smtp_port = int(os.getenv("SMTP_PORT", 587))
         self.brevo_smtp_username = os.getenv("BREVO_SMTP_USERNAME")
